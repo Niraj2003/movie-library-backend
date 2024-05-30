81bdd046f1ff860eb34d606609173243a4b4b438
Niraj Amrutkar <90129030+Niraj2003@users.noreply.github.com>
2024-05-31 01:46:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://movie-library-sooty.vercel.app/',
+  origin: '*',
   methods: ['GET', 'POST'],
   credentials: true
 }));
