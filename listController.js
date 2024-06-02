1871af9be9e1dcf6f166f898d154b00b8072c9ba
Niraj Amrutkar <>
2024-06-03 02:38:05 +0530

    chore: Populate user information in getPublicLists endpoint

--- listController.js
+++ listController.js
@@ -31,10 +31,16 @@ exports.getLists = async (req, res) => {
 exports.getPublicLists = async (req, res) => {
   console.log('Retrieving public lists');
   try {
-    const lists = await List.find({ isPublic: true });
+    const lists = await List.find({ isPublic: true }).populate('user', 'username email');;
     console.log('Public lists retrieved:');
+    const listsWithUsername = lists.map(list => {
+      const { _id, name, movies, isPublic, user } = list;
+      const username = user.username;
+      return { _id, name, movies, isPublic, user: username };
+    });
+    console.log(listsWithUsername);
     // console.log(lists);
-    res.json(lists);
+    res.json(listsWithUsername);
   } catch (error) {
     console.error('Error retrieving public lists:', error);
     res.status(500).json({ message: 'Server error' });
